d0876b9dab7b9a37aa7cd6f58677e31948305ba0
kinan <kinan2392@gmail.com>
2023-08-25 23:24:39 +0600

    responsive side-nav

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 import './style.scss'
 
+
+const menu = document.querySelector('#menu');
+const drawer = document.querySelector('.left-drawer');
+menu.addEventListener('click', () => {
+  if (drawer.style.display == "none") {
+    drawer.style.display = "block";
+  } else {
+    drawer.style.display = "none";
+  }
+})
+
+const myFunction = x => {
+  if (x.matches) {
+    drawer.style.display = "none";
+  } else {
+    drawer.style.display = "block";
+  }
+};
+
+const x = window.matchMedia("(max-width: 700px)");
+myFunction(x);
+x.addEventListener("change", myFunction);
